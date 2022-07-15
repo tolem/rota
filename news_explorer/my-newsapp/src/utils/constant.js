1efd087df902f2b5402c880580090ea3eee342fd
Lami <41260099+tolem@users.noreply.github.com>
2022-07-15 19:42:59 -0400

    Delete news_explorer/my-newsapp directory
    Netlify needs parent dir

--- news_explorer/my-newsapp/src/utils/constant.js
+++
@@ -1,2 +0,0 @@
-/* eslint-disable import/prefer-default-export */
-export const Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
